92a77fb02981a0cd73e91d3531839af3b3f5b1fd
andyzf <494126372@qq.com>
2019-06-06 16:28:03 +0800

    localdb 保存ef-core 创建的库与local sqlserver 链接的库的区别

--- MyMvc/Models/Movie.cs
+++ MyMvc/Models/Movie.cs
@@ -18,6 +18,7 @@ namespace MyMvc.Models
         [Display(Name = "发布 Date")]  //显示的字段名 而不是字段表中名称
         [DataType(DataType.Date)]  // 只显示到天 中文类型
         public DateTime ReleaseDate { get; set; }
+
         // 分类 流派
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]  // 校验
         [Required]  // 必填
